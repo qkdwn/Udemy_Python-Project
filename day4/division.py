9143c410738836f9e8ee363069a9109c03812781
Winter <bjhbjh0313@gmail.com>
2024-07-16 13:42:06 +0900

    modified:   day4/division.py

--- day4/division.py
+++ day4/division.py
@@ -9,7 +9,6 @@ dirty_dozen = [fruits, vegetables]
 
 print(dirty_dozen[0][0])
 
-
 #dirty_dozen[0][0]은 fruits, strawberries를 나타내고
 #[1][0]은 vegetables, spinach를 나타낸다.
 #즉 앞에는 과일인지 채소인지를 묻는 리스트이고, 뒤에 부분은 해당 리스트에 포함된 값을 의미한다.
