808c42716840ebfee5a7e8ac20be8d05310e4208
Winter <bjbjh0313@gmail.com>
2024-07-23 11:51:18 +0900

    modified:   day5/Adding_even_numbers.py 	new file:   day5/FIZZBUZZ.py

--- day5/Adding_even_numbers.py
+++ day5/Adding_even_numbers.py
@@ -1,7 +1,12 @@
-num1 = int(input())
-num2 = int(input())
+target = int(input())
 
-print(num1 * (num2%10))
-print(num1 * ((num2%100)//10))
-print(num1 * (num2//100))
-print(num1 * num2)
+even_sum = 0
+for number in range(2, target + 1, 2): #특정 숫자까지의 범위를 만들려면 해당 숫자에 1을 더한 값을 넣어야 한다.
+    even_sum += number
+print(even_sum)
+
+alternative_sum = 0
+for number in range(1, target + 1):
+    if number % 2 == 0:
+        alternative_sum += number
+print(alternative_sum)
