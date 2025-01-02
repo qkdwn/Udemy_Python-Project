f24d2df0d60fd2d92968303d16b322b35cf45e28
Winter <bjhbjh0313@gmail.com>
2025-01-02 14:52:48 +0900

    Day10

---
+++ Day9/bidding.py
@@ -0,0 +1,4 @@
+import logo from art.py
+print("Hello")
+print("\n"*100)
+unput("Type something")
---
+++ Day9/task.py
@@ -0,0 +1,30 @@
+capitals = {
+    "France": "Paris",
+    "Germany": "Berlin"
+}
+
+#travel_log = {
+#    "France": ["Paris","Lille","Dijon"],
+#    "Germany": ["Stuttgart","Berlin"]
+#}
+
+#print Lille
+#print(travel_log["France"][1])
+
+
+# print D
+nested_list = ["A","B",["C","D"]]
+# print(nested_list[2][1])
+
+travel_log = {
+    "France": {
+        "cities_visited": ["Paris","Lille","Dijon"],
+        "total_visits": 12
+    },
+    "Germany": {
+        "cities_visited": ["Berlin","Hamburg","Stuttgart"],
+        "total_visits": 5
+    },
+}
+
+print(travel_log["Germany"]["cities_visited"][2])
