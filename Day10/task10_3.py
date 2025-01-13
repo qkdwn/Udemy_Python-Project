a36f129aad3c2d1cd01942c6bf5304e01c6b7083
Winter <bjhbjh0313@gmail.com>
2025-01-13 12:55:32 +0900

    Day10

--- Day10/task10_3.py
+++ Day10/task10_3.py
@@ -8,3 +8,4 @@ def format_name(f_name, l_name):
     #print("This got printed")는 실행이 안된다 왜냐하면 return이 컴퓨터에게 이것이 함수의 끝임을 알려주고 이제 함수를 종료해야 한다고 말하기 때문
 
 print(format_name(input("What is your first name?"), input("What is your last name?")))
+s
