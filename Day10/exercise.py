e675ebf38af078cc9c13661e479870cec1a4f4f9
Winter <bjhbjh0313@gmail.com>
2025-01-13 13:25:01 +0900

    Day10

--- Day10/exercise.py
+++ Day10/exercise.py
@@ -8,8 +8,9 @@ year = int(input(""))
 
 print(is_leap_year(year))
 
-#udemy 해설
-#def is_leap_year(year):
+# udemy 해설
+# 전체 주석 처리는 ctrl + /
+# def is_leap_year(year):
 #    if year % 4 == 0:
 #        if year % 100 == 0:
 #            if year % 400 == 0:
