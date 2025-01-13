94c1ba9067a3a0083d0bd6461c41101495a9559e
Winter <bjhbjh0313@gmail.com>
2025-01-13 13:18:04 +0900

    Day10

---
+++ Day10/exercise.py
@@ -0,0 +1,22 @@
+def is_leap_year(year):
+    if(year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        return True
+    else:
+        return False
+
+year = int(input(""))
+
+print(is_leap_year(year))
+
+#udemy 해설
+#def is_leap_year(year):
+#    if year % 4 == 0:
+#        if year % 100 == 0:
+#            if year % 400 == 0:
+#                return True
+#            else:
+#                return False
+#        else:
+#            return True
+#    else:
+#        return False
